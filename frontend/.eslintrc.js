5746fe9d4f274d1cb3b70426ad30af5cd0cf99d0
권혁진 <khj9709@icloud.com>
2022-07-19 00:46:46 +0900

    [CHORE] 개발환경 세팅하기 (FE, Github) (#2)
    * chore: 📦이슈, PR 템플릿 설정 #1
    
    * chore: :package: webpack, lint, prettier 세팅 #1

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:prettier/recommended', 'eslint:recommended', 'plugin:import/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': ['error', { varsIgnorePattern: '_', argsIgnorePattern: '_' }],
+    'no-console': 'warn',
+    'import/no-duplicates': ['error'],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.js',
+      },
+    },
+  },
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,78 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require('path');
+
+const isProductionMode = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProductionMode ? 'production' : 'development',
+  entry: path.resolve(__dirname, './src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[hash].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: {
+                    version: 3,
+                  },
+                  targets: {
+                    browsers: ['>= 1%, not dead'],
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        use: [
+          isProductionMode ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(png|jpe?g|gif|otf|ttf)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    hot: true,
+  },
+  devtool: 'eval-source-map',
+};
