720045f0b07b8f514a180f6e7169c1cb5cbfef95
jstella96 <jstella0530@naver.com>
2022-07-21 00:49:15 +0900

    feat: :sparkles: database 연결 #7

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
-const exp = require("constants");
-const Router = require("./routes");
-
+const express = require('express');
+const path = require('path');
+const exp = require('constants');
+const Router = require('./routes');
+const { initDatabase } = require('./lib/database.js');
 const PORT = process.env.PORT || 3000;
+
+initDatabase();
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api", Router);
+app.use('/api', Router);
 
 app.listen(PORT);
 
-console.log("-------------");
-console.log("listeneing ... " + PORT);
-console.log("-------------");
+console.log('-------------');
+console.log('listeneing ... ' + PORT);
+console.log('-------------');
 
 module.exports = app;
