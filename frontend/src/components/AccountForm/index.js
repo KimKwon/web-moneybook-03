79bb86db111501e2bdf890145aa8fa9ff01a78d6
jstella96 <jstella0530@naver.com>
2022-07-24 10:15:46 +0900

    feat: :sparkles: accountForm변경함수 test

--- frontend/src/components/AccountForm/index.js
+++ frontend/src/components/AccountForm/index.js
@@ -20,7 +20,7 @@ class AccountForm extends Component {
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">내용</span>
-                <input class="account-form-input" placeholder="입력해주세요"/>
+                <input class="account-form-input content" data-name="content"  placeholder="입력해주세요"/>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">결제수단</span>
@@ -30,7 +30,7 @@ class AccountForm extends Component {
                 <span class="account-form-label">금액</span>
                 <div class="account-form-amount">
                   <span><img src="${lineIcon}"/></span>
-                  <input  class="account-form-input" placeholder="입력해주세요"/>
+                  <input  class="account-form-input amount" placeholder="입력해주세요"/>
                 </div>
             </div>
             <button class="account-form-button"><img src="${saveButtonIcon}"/></button>
@@ -40,6 +40,13 @@ class AccountForm extends Component {
   reFatchFormData(newFormData) {
     console.log(newFormData);
     const { categoryId, methodId, content, amount, date } = newFormData;
+    this.$formContent.value = content;
+    this.$formAmount.value = amount;
+  }
+  didMount() {
+    /* 테스트 */
+    this.$formContent = document.querySelector('.content');
+    this.$formAmount = document.querySelector('.amount');
   }
   render() {
     const template = this.template();
