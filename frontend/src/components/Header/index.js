4606053d9e2cc48fd6712aef5162775517ca68f9
Jinny <khj9709@icloud.com>
2022-07-24 19:01:17 +0900

    feat: :sparkles: url에 따라 네브바 아이콘 색상 변경 #23

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -6,20 +6,37 @@ import chartIcon from '@/assets/icon/chart.svg';
 import MonthController from './monthController';
 import Component from '@/lib/component';
 
+const NAV_INFO = [
+  { className: 'nav-main', href: '/', icon: fileTextIcon },
+  { className: 'nav-calendar', href: '/calendar', icon: calendarIcon },
+  { className: 'nav-statistic', href: '/statistic', icon: chartIcon },
+];
 export default class Header extends Component {
   constructor($target) {
     super($target);
   }
 
+  isNavItemActive(targetHref) {
+    return location.pathname === targetHref;
+  }
+
+  showNavItem() {
+    return NAV_INFO.map(
+      ({ className, href, icon }) => `
+            <a is="my-anchor" class="${
+              className + (this.isNavItemActive(href) ? ' active' : '')
+            }" href=${href}>${icon}</a>
+          `,
+    ).join('');
+  }
+
   template() {
-    return `
+    return /*html*/ `
       <div class="header">
         <a is="my-anchor" href="/" class="header-title" >우아한 가계부</a>
         <div class="month-controller" ></div>
         <div class="nav">
-          <a class="nav-main" is="my-anchor" href="/">${fileTextIcon}</a> 
-          <a class="nav-calendar" is="my-anchor" href="/calendar">${calendarIcon}</a>
-          <a class="nav-statistic" is="my-anchor" href="/statistic">${chartIcon}</a>
+          ${this.showNavItem()}
         </div>
       </div>
     `;
