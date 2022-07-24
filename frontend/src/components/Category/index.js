00bdad6eb79b5714dd447fa45f0a2fd24d64d25b
jstella96 <jstella0530@naver.com>
2022-07-25 08:53:15 +0900

    feat: :sparkles: category component 부모요소에 render()  함수 추가

--- frontend/src/components/Category/index.js
+++ frontend/src/components/Category/index.js
@@ -6,35 +6,32 @@ import { SELECTOR_MAP } from '@/constants/selector-map';
 class Category extends Component {
   constructor($target, initialState) {
     super($target, initialState);
+  }
+  init() {
     this.$element = document.createElement('div');
     this.$element.className = 'category-tag';
   }
-
-  template() {
-    return /*html*/ `
-        <div class="category-tag">
-            <span>태그 샘플 ${categoryId}</span>
-        </div>
-    `;
-  }
-
   didMount() {
     store.subscribe(SELECTOR_MAP.CATEGORY, this.paintCategory.bind(this));
   }
 
   paintCategory() {
     /* category 배열이 아닌 객체리터럴이여도 좋을것 같네요 */
-    const categories = store.getState(SELECTOR_MAP.CATEGORY);
+    let categories = store.getState(SELECTOR_MAP.CATEGORY);
+    categories = [
+      ...categories.expenditureCategoryDummyData,
+      ...categories.incomeCategoryDummyData,
+    ];
     const category = categories.filter((category) => {
-      return category.id == this.state.categoryId;
+      return category.id == this.state.id;
     });
-    this.$element.style.backgroundColor = category.color;
+    this.$element.style.backgroundColor = category[0].color;
   }
 
   render() {
     this.$element.innerHTML = this.state.name;
     this.paintCategory();
-    this.$target.appendElement(this.$element);
+    this.$target.appendChild(this.$element);
   }
 }
 
