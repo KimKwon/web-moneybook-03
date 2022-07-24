e2415c6feddbfca295d630c07f741a0a1d8ec8bd
jstella96 <jstella0530@naver.com>
2022-07-24 21:21:49 +0900

    refactor: :hammer: dayToString함수 utils로 이동

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -11,3 +11,16 @@ export const updateDate = (currentDate, nextMonth) => {
   }
   return nextDate;
 };
+
+export const dayToString = (day) => {
+  const dayMap = {
+    0: '월',
+    1: '화',
+    2: '수',
+    3: '목',
+    4: '금',
+    5: '토',
+    6: '일',
+  };
+  return dayMap[day];
+};
