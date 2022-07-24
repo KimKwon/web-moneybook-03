75a1b2bf110c0bb78849b539edb056d6a22a6356
Jinny <khj9709@icloud.com>
2022-07-24 22:49:08 +0900

    feat: :sparkles: 드랍다운 select로 임시 구현 #34

--- frontend/src/components/AccountForm/index.js
+++ frontend/src/components/AccountForm/index.js
@@ -2,6 +2,12 @@ import Component from '@/lib/component';
 import './index.scss';
 import saveButtonIcon from '@/assets/icon/save-button.svg';
 import lineIcon from '@/assets/icon/line.svg';
+import store from '@/store/index';
+import { SELECTOR_MAP } from '@/constants/selector-map';
+
+const INCOME = 'income';
+const EXPENDITURE = 'expenditure';
+
 class AccountForm extends Component {
   constructor($target) {
     super($target, {
@@ -10,12 +16,48 @@ class AccountForm extends Component {
         category: '',
         content: '',
         method: '',
-        amount: '',
+        amount: -13133113,
       },
+      currentCategory: EXPENDITURE,
     });
   }
+
+  showMethods() {
+    const methods = store.getState(SELECTOR_MAP.PAYMENT_METHODS);
+    return methods
+      .map(
+        ({ name }) => `
+        <option>
+          ${name}
+        </option>
+      `,
+      )
+      .join('');
+  }
+
+  showCategoryOptions() {
+    const { currentCategory } = this.state.accountInfo;
+    const category = store.getState(SELECTOR_MAP.CATEGORY);
+
+    const categories =
+      currentCategory === INCOME
+        ? category.incomeCategoryDummyData
+        : category.expenditureCategoryDummyData;
+
+    return categories
+      .map(
+        ({ name }) => `
+        <option>
+          ${name}
+        </option>
+      `,
+      )
+      .join('');
+  }
+
   template() {
-    const initialDate = this.state.accountInfo.date.getParsedDatestring('YYYY-MM-DD');
+    const { date, category, method, amount, content } = this.state.accountInfo;
+    const initialDate = date.getParsedDatestring('YYYY-MM-DD');
     return /*html*/ `
         <form class="account-form">
             <div class="account-form-wrapper">
@@ -24,21 +66,30 @@ class AccountForm extends Component {
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">분류</span>
-                <div class="account-form-input">선택하세요</div>
+                <select>
+                  <option selected disabled class="account-form-input">선택하세요</option>
+                  ${this.showCategoryOptions()}
+                </select>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">내용</span>
-                <input class="account-form-input content" data-name="content"  placeholder="입력해주세요"/>
+                <input class="account-form-input content" value="${content}" data-name="content"  placeholder="입력하세요"/>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">결제수단</span>
-                <div class="account-form-input">선택하세요</div>
+                <select>
+                  <option selected disabled class="account-form-input">선택하세요</option>
+                  ${this.showMethods()}
+                </select>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">금액</span>
                 <div class="account-form-amount">
                   <span>${lineIcon}</span>
-                  <input class="account-form-input amount" placeholder="입력해주세요"/>
+                  <input value=${
+                    amount === '' || Number(amount).toLocaleString()
+                  } class="account-form-input amount" placeholder="입력하세요"/>
+                  <span>원</span>
                 </div>
             </div>
             <button type="submit" class="account-form-button">${saveButtonIcon}</button>
