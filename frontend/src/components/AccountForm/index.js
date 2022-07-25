bab563bc23cf0776dbdddc70efd7ab605ab7ff63
권혁진 <khj9709@icloud.com>
2022-07-25 12:39:33 +0900

    [FEATURE] AccountForm 컴포넌트 구현 구체화 (#38)
    * feature: :sparkles: 날짜 유틸함수 추가 #34
    
    * feature: :sparkles: form 초기 날짜값 할당 #34
    
    * feat: :sparkles: 드랍다운 select로 임시 구현 #34
    
    * feat: :sparkles: form 카테고리 토글 추가 #34
    
    * feat: :sparkles: 히스토리 액션(추가/수정) 추가 #34
    
    * chore: :package: shortid 의존성 추가 #34
    
    * feat: :sparkles: 히스토리 추가 기능 구현 #34
    
    * feat: :sparkles: 히스토리 클릭시 불러오기 #34
    
    * feat: :sparkles: 히스토리 수정 기능 구현 #34
    
    * chore: :package: 초기데이터 상수화 #34
    
    * edit: :fire: date util에 default case 추가 #34

--- frontend/src/components/AccountForm/index.js
+++ frontend/src/components/AccountForm/index.js
@@ -2,54 +2,192 @@ import Component from '@/lib/component';
 import './index.scss';
 import saveButtonIcon from '@/assets/icon/save-button.svg';
 import lineIcon from '@/assets/icon/line.svg';
+import plusIcon from '@/assets/icon/plus.svg';
+import store from '@/store/index';
+import { SELECTOR_MAP } from '@/constants/selector-map';
+import shortid from 'shortid';
+
+const INCOME = 'income';
+const EXPENDITURE = 'expenditure';
+
+const INITIAL_FORM_DATA = {
+  id: '',
+  date: new Date().toString(),
+  category: '',
+  content: '',
+  method: '',
+  amount: '',
+};
 
 class AccountForm extends Component {
-  constructor($target, initialState) {
-    super($target, initialState);
+  constructor($target) {
+    super($target, {
+      accountInfo: INITIAL_FORM_DATA,
+      currentCategoryType: INCOME,
+      isEditMode: false,
+    });
   }
+
+  initForm() {
+    this.setState({
+      accountInfo: INITIAL_FORM_DATA,
+      currentCategoryType: EXPENDITURE,
+      isEditMode: false,
+    });
+  }
+
+  showMethods() {
+    const methods = store.getState(SELECTOR_MAP.PAYMENT_METHODS);
+    const { method } = this.state.accountInfo;
+    return methods
+      .map(
+        ({ name }) => `
+        <option ${method === name ? 'selected' : ''}>
+          ${name}
+        </option>
+      `,
+      )
+      .join('');
+  }
+
+  showCategoryOptions() {
+    const { currentCategoryType, accountInfo } = this.state;
+    const { category } = accountInfo;
+    const originCategory = store.getState(SELECTOR_MAP.CATEGORY);
+
+    const categories =
+      currentCategoryType === INCOME
+        ? originCategory.incomeCategoryDummyData
+        : originCategory.expenditureCategoryDummyData;
+
+    return categories
+      .map(
+        ({ name }) => `
+        <option ${category === name ? 'selected' : ''}>
+          ${name}
+        </option>
+      `,
+      )
+      .join('');
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    const $date = this.$form.querySelector('.account-form-input.date');
+    const $category = this.$form.querySelector('.account-form-dropdown.category');
+    const $content = this.$form.querySelector('.account-form-input.content');
+    const $method = this.$form.querySelector('.account-form-dropdown.method');
+    const $amount = this.$form.querySelector('.account-form-input.amount');
+
+    const { isEditMode, accountInfo } = this.state;
+    const { id } = accountInfo;
+
+    store.dispatch(
+      isEditMode ? 'updateAccountHistory' : 'addAccountHistory',
+      {
+        id: id || shortid(),
+        date: new Date($date.value),
+        content: $content.value,
+        amount: Number($amount.value.toString().replaceAll(',', '')),
+        methodName: $method.selectedOptions[0]?.value,
+        categoryName: $category.selectedOptions[0]?.value,
+        categoryId: shortid(),
+        isProfit: true,
+      },
+      SELECTOR_MAP.ACCOUNT_HISTORY,
+    );
+
+    this.initForm();
+  }
+
   template() {
+    const { currentCategoryType, accountInfo } = this.state;
+    const { date, amount, content } = accountInfo;
+    const initialDate = date.getParsedDatestring('YYYY-MM-DD');
     return /*html*/ `
         <form class="account-form">
             <div class="account-form-wrapper">
                 <span class="account-form-label">날짜</span>
-                <input type="date" class="account-form-input" placeholder="입력해주세요" />
+                <input type="date" value=${initialDate} class="account-form-input date" placeholder="입력해주세요" />
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">분류</span>
-                <div class="account-form-input">선택하세요</div>
+                <select class="account-form-dropdown category">
+                  <option selected disabled>선택하세요</option>
+                  ${this.showCategoryOptions()}
+                </select>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">내용</span>
-                <input class="account-form-input content" data-name="content"  placeholder="입력해주세요"/>
+                <input class="account-form-input content" value="${content}" data-name="content"  placeholder="입력하세요"/>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">결제수단</span>
-                <div class="account-form-input">선택하세요</div>
+                <select class="account-form-dropdown method"> 
+                  <option selected disabled >선택하세요</option>
+                  ${this.showMethods()}
+                </select>
             </div>
             <div class="account-form-wrapper">
                 <span class="account-form-label">금액</span>
                 <div class="account-form-amount">
-                  <span><img src="${lineIcon}"/></span>
-                  <input  class="account-form-input amount" placeholder="입력해주세요"/>
+                  <button type="button" class="account-form-amount__toggleButton">
+                    ${currentCategoryType === INCOME ? plusIcon : lineIcon}
+                  </button>
+                  <input
+                    value="${amount === '' ? amount : Number(amount).toLocaleString()}"
+                    class="account-form-input amount" placeholder="입력하세요"
+                  />
+                  <span>원</span>
                 </div>
             </div>
-            <button class="account-form-button"><img src="${saveButtonIcon}"/></button>
+            <button type="submit" class="account-form-button">${saveButtonIcon}</button>
         </form>
     `;
   }
+
+  attachEvent() {
+    this.$form.addEventListener('submit', this.handleFormSubmit.bind(this));
+
+    const toggleButton = this.$form.querySelector('.account-form-amount__toggleButton');
+    toggleButton.addEventListener('click', () => {
+      const { currentCategoryType } = this.state;
+      const nextCategory = currentCategoryType === INCOME ? EXPENDITURE : INCOME;
+      this.setState({ currentCategoryType: nextCategory });
+    });
+  }
+
   reFatchFormData(newFormData) {
-    const { categoryId, methodId, content, amount, date } = newFormData;
-    this.$formContent.value = content;
-    this.$formAmount.value = amount;
+    const { id, content, amount, date, categoryName, methodName } = newFormData;
+
+    this.setState({
+      accountInfo: {
+        ...this.state.accountInfo,
+        id,
+        category: categoryName,
+        method: methodName,
+        content,
+        amount,
+        date: date.toString(),
+      },
+      isEditMode: true,
+    });
   }
-  didMount() {
-    /* 테스트 */
-    this.$formContent = document.querySelector('.content');
-    this.$formAmount = document.querySelector('.amount');
+
+  afterRender() {
+    this.$form = this.$target.querySelector('.account-form');
+    this.attachEvent();
   }
+
   render() {
     const template = this.template();
-    this.$target.insertAdjacentHTML('beforeend', template);
+    if (this.$form) {
+      this.$form.remove();
+      this.$form = null;
+    }
+    this.$target.insertAdjacentHTML('afterBegin', template);
+
+    this.afterRender();
   }
 }
 export default AccountForm;
