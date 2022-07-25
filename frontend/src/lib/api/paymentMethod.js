278b9eb55dd8ffbffb623d59e67e62da4ce56ae7
jstella96 <jstella0530@naver.com>
2022-07-26 01:30:25 +0900

    feat: :sparkles:  putPaymentMethod, deletePaymentMethod 함수 구현, request 요청

--- frontend/src/lib/api/paymentMethod.js
+++ frontend/src/lib/api/paymentMethod.js
@@ -17,3 +17,33 @@ export const getPaymentMethod = async () => {
     return null;
   }
 };
+
+export const putPaymentMethod = async (paymentMethod) => {
+  const option = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(paymentMethod),
+  };
+  try {
+    const response = await fetch(`${baseUrl}/payment-method`, option);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const deletePaymentMethod = async (id) => {
+  const option = {
+    method: 'DELETE',
+  };
+  try {
+    const response = await fetch(`${baseUrl}/payment-method/${id}`, option);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
+};
