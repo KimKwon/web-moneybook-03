f0f462bb45fab2176476198096c77d0c8b1b23e5
jstella96 <jstella0530@naver.com>
2022-07-27 11:11:05 +0900

    refactor: :hammer: 테스트 버튼 추가

--- frontend/src/pages/Statistic.js
+++ frontend/src/pages/Statistic.js
@@ -5,8 +5,15 @@ function Statistic($root) {
   const $header = $root.querySelector('header');
   const $main = $root.querySelector('main');
   new Header($header);
-  const initialState = { month: 7, year: 2022, categoryId: 1, period: 6 };
-  new BarChartContainer($main, initialState);
+  const initialState = { month: 7, year: 2022, categoryId: 1, period: 12 };
+  const $barChart = new BarChartContainer($main, initialState);
+
+  const $testButton = document.createElement('button');
+  $testButton.innerHTML = 'testButton';
+  $testButton.addEventListener('click', () => {
+    $barChart.setState({ month: 6, year: 2022, categoryId: 1, period: 3 });
+  });
+  $root.appendChild($testButton);
 }
 
 export default Statistic;
