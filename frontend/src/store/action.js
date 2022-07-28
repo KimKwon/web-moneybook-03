560debf2670ef307fac95f51267d716421c7ebc8
Jinny <khj9709@icloud.com>
2022-07-29 00:47:13 +0900

    refactor: :hammer: 액션 이름 상수로 분리 #46

---
+++ frontend/src/store/action.js
@@ -0,0 +1,13 @@
+export const SET_CATEGORY = 'category/setCategory';
+
+export const SET_PATMENT_METHOD = 'payment/setPaymentMethod';
+export const ADD_PAYMENT_METHOD = 'payment/addPaymentMethod';
+export const REMOVE_PAYMENT_METHOD = 'payment/removePaymentMethod';
+
+export const SET_ACCOUNT_HISTORY = 'setAccountHistory';
+export const ADD_ACCOUNT_HISTORY = 'addAccountHistory';
+export const UPDATE_ACCOUNT_HISTORY = 'updateAccountHistory';
+
+export const CHANGE_LOADING_STATE = 'changeLoadingState';
+
+export const UPDATE_MONTH = 'updateMonth';
