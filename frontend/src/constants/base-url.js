bc6e3bfbe31f66731594c1afdf9c299c78a24c95
권혁진 <khj9709@icloud.com>
2022-07-28 21:18:07 +0900

    bug: :bug: 배포환경 base-url 수정 (#71)

--- frontend/src/constants/base-url.js
+++ frontend/src/constants/base-url.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/api';
+export const baseUrl = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
