6028f61cc47b12c6695923bfc503a90a05c830e0
Jinny <khj9709@icloud.com>
2022-07-29 00:50:18 +0900

    edit: :fire: 액션이름 prefix 일관성 있게 변경 #46

--- frontend/src/store/action.js
+++ frontend/src/store/action.js
@@ -4,10 +4,10 @@ export const SET_PATMENT_METHOD = 'payment/setPaymentMethod';
 export const ADD_PAYMENT_METHOD = 'payment/addPaymentMethod';
 export const REMOVE_PAYMENT_METHOD = 'payment/removePaymentMethod';
 
-export const SET_ACCOUNT_HISTORY = 'setAccountHistory';
-export const ADD_ACCOUNT_HISTORY = 'addAccountHistory';
-export const UPDATE_ACCOUNT_HISTORY = 'updateAccountHistory';
+export const SET_ACCOUNT_HISTORY = 'history/setAccountHistory';
+export const ADD_ACCOUNT_HISTORY = 'history/addAccountHistory';
+export const UPDATE_ACCOUNT_HISTORY = 'history/updateAccountHistory';
 
-export const CHANGE_LOADING_STATE = 'changeLoadingState';
+export const CHANGE_LOADING_STATE = 'loading/changeLoadingState';
 
-export const UPDATE_MONTH = 'updateMonth';
+export const UPDATE_MONTH = 'month/updateMonth';
